49193d7fc996b23eb58c536a94956c0058f9c27d
y13186 <yu.peipei@h3c.com>
2017-08-22 16:21:56 +0800

    organize source files

---
+++ scripts/utils/openBrowser.js
@@ -0,0 +1,25 @@
+var execSync = require("child_process").execSync;
+var opn = require("opn");
+
+function openBrowser(url) {
+	if (process.platform === "darwin") {
+		try {
+			execSync("ps cax | grep 'Google Chrome'");
+			execSync(
+				"osascript openChrome.applescript " + url,
+				{cwd: __dirname, stdio: "ignore"}
+			);
+			return true;
+		} catch (err) {
+
+		}
+	}
+	try {
+		opn(url).catch(() => {});
+		return true;
+	} catch (true) {
+		return false;
+	}
+}
+
+module.exports = openBrowser;
