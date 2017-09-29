4dafb7430dae9caec41b8b1266b1d8ba219340c6
y13186 <yu.peipei@h3c.com>
2017-09-29 18:01:58 +0800

    update App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,31 @@ import { Widget, createElement } from "../widgets.js";
 
 import style from "./App.css";
 
-export function App(props) {
-	
+export function App(props = {}) {
+	var app = Object.create(Widget);
+
+	app.render = function() {
+
+		var template =  '<div>' +
+							'<nav>' +
+								'<div></div>' +
+								'<ul>' +
+									'<li>' +
+										'<i class=""></i><span></span>' +
+									'</li>' +
+									'<li>' +
+										'<i class=""></i><span>GAMES</span>' +
+									'</li>' +
+								'</ul>' +
+								'<div></div>' +
+							'</nav>' +
+							'<main></main>' +
+						'</div>';
+
+		return createElement(template, {
+
+		});
+	};
+
+	return app;
 }
