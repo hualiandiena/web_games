05f363c620e416be13b9507e0739aba3a00c24fe
y13186 <yu.peipei@h3c.com>
2017-09-30 17:55:31 +0800

    just push the code to github

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,47 @@ import { Widget, createElement } from "../widgets.js";
 
 import style from "./App.css";
 
+function Welcome(props = {}) {
+	var template =  '<div>' +
+						'' +
+					'</div>';
+
+	return createElement(template, {});
+}
+
 export function App(props = {}) {
 	var app = Object.create(Widget);
 
+	app.state = {
+		welcome: true
+	};
+
 	app.render = function() {
 
+		var welcome = app.state.welcome ? Welcome() : null;
 		var template =  '<div>' +
+							'{{welcome}}' +
 							'<nav>' +
 								'<div></div>' +
 								'<ul>' +
 									'<li>' +
-										'<i class=""></i><span></span>' +
+										'<i class=""></i><span>Navigator</span>' +
 									'</li>' +
 									'<li>' +
 										'<i class=""></i><span>GAMES</span>' +
 									'</li>' +
+									'<li>' +
+										'<i></i><span>User</span>' +
+									'</li>' +
 								'</ul>' +
 								'<div></div>' +
 							'</nav>' +
-							'<main></main>' +
+							'<main>' +
+							'</main>' +
 						'</div>';
 
 		return createElement(template, {
-
+			welcome
 		});
 	};
 
